53f822d0ba9362bd2df63c84f6103c2bfdc771fb
Mitch McCullough <mitchmccullough@gmail.com>
2023-03-22 22:36:49 -0500

    incorporation

--- seed.sh
+++ seed.sh
@@ -3,21 +3,8 @@
 # Run chmod +x seed.sh in the terminal.
 # run ./seed.sh in the terminal to run the commands
 
-rm -rf vacationistaapi/migrations
-rm db.sqlite3
-python3 manage.py migrate
-python3 manage.py makemigrations vacationistaapi
-python3 manage.py migrate vacationistaapi
-python3 manage.py loaddata users
-python3 manage.py loaddata articles
-python3 manage.py loaddata recommendations
-python3 manage.py loaddata highlights
-python3 manage.py loaddata event_types
-python3 manage.py loaddata expense_types
-python3 manage.py loaddata transportation_types
-python3 manage.py loaddata legs
-python3 manage.py loaddata trips
-python3 manage.py loaddata events
-python3 manage.py loaddata expenses
-python3 manage.py loaddata transportations
-python3 manage.py loaddata trip_legs
+heroku pg:reset --app vacationista --confirm vacationista
+heroku run python3 manage.py migrate --app vacationista
+heroku run python3 manage.py makemigrations vacationistaapi --app vacationista
+heroku run python3 manage.py migrate vacationistaapi --app vacationista
+heroku run python3 manage.py loaddata users articles recommendations highlights event_types expense_types transportation_types legs trips events expenses transportations trip_legs --app vacationista
