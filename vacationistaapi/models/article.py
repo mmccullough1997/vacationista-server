ed1a046e3d2bcc8a99fd6b52e9999e08a4979360
Mitch McCullough <mitchmccullough@gmail.com>
2023-03-22 20:18:45 -0500

    added

--- vacationistaapi/models/article.py
+++ vacationistaapi/models/article.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Article(models.Model):
-  title = models.CharField(max_length=50000)
+  title = models.CharField(max_length=50)
   date_posted = models.DateField()
-  content = models.CharField(max_length=50000)
-  image = models.CharField(max_length=50000)
-  thumbnail = models.CharField(max_length=50000)
+  content = models.CharField(max_length=255)
+  image = models.CharField(max_length=255)
+  thumbnail = models.CharField(max_length=255)
